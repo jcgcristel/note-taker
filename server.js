7b00c91db9203e7d092e533e6042d35c4bfb725f
jcg <jcg.cristel@gmail.com>
2022-07-19 13:24:50 -0400

    routed front end

--- server.js
+++ server.js
@@ -3,6 +3,15 @@ const express = require('express');
 const PORT = process.env.PORT || 3001;
 const app = express();
 
+// Required Routes
+const htmlRoutes = require('./routes/htmlRoutes');
+
+app.use(express.json());
+app.use(express.static('public'));
+
+// Routes
+app.use('/', htmlRoutes);
+
 app.listen(PORT, () => {
-    console.log(`API Server open on port ${PORT}`);
+    console.log(`API Server open on port ${PORT}.`);
 });
